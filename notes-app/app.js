435ae6eacf0c05045c6de77e3d5b18340a6f30bb
Satyam <satyambhardawaj@gmail.com>
2020-10-17 10:57:26 +0530

    export functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,21 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'This file was created by Satyam');
+// fs.writeFileSync('notes.txt', 'This file was created by Satyam');
 
-//Challenge 1
-fs.appendFileSync('notes.txt', ' Data is appended in this file')
+// //Challenge 1
+// fs.appendFileSync('notes.txt', ' Data is appended in this file')
+
+// const lastname = require('./utils');
+
+// const name = 'Satyam'
+
+// const add = require('./utils')
+
+// const sum = add(2, 5)
+
+// console.log(sum);
+
+// Challenge 2
+const getNotes = require('./notes');
+const message = getNotes()
+console.log(message);
