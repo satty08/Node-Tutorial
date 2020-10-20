938fa78ca191ce8ec75e2248c868a206933c03f2
Satyam <satyambhardawaj@gmail.com>
2020-10-20 13:03:22 +0530

    playground

---
+++ playground/1-json.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday'
+// }
+
+// const bookJson = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJson)
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJson = dataBuffer.toString();
+// const data = JSON.parse(dataJson)
+// console.log(data.title);
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+
+// data.age = 22
+// data.name = 'Satyam'
+// const userJson = JSON.stringify(data);
+// fs.writeFileSync('1-json.json', userJson)
---
+++ playground/2-arrow-functions.js
@@ -0,0 +1,23 @@
+// const square = function(x) {
+//     return x*x
+// }
+
+// const square = (x) => {return x*x}
+
+// const square = (x) => x*x
+
+// console.log(square(3));
+
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Tarun', 'Aparna', 'Kunal'],
+    printGuestList() {
+        console.log('Guest list for ' + this.name);
+
+        this.guestList.forEach(guest => {
+            console.log(guest + ' is coming to the ' + this.name );
+        })
+    }
+}
+
+event.printGuestList()
