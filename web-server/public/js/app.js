a90943f77db711b608ef086646729fd066db4b63
Satyam <satyambhardawaj@gmail.com>
2020-10-31 19:54:02 +0530

    final version

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,28 @@
-console.log('Client side JavaScript is loaded');
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const output1 = document.querySelector('#message1')
+const output2 = document.querySelector('#message2')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    output1.textContent = 'Loading...'
+    output2.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+        res.json().then((data) => {
+            if (data.error) {
+                output1.textContent = data.error
+                
+            }else{
+                output1.textContent = data.location
+                output2.textContent = data.forecast
+            }
+        })
+    })    
+})
