8c6702b3dd19aa412a36527778aed76b6b5c85ee
Satyam <satyambhardawaj@gmail.com>
2020-11-16 12:30:30 +0530

    working with postman

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid!')
+            }
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        validate(value) {
+            if (value.length < 6) {
+                throw new Error('Password length must be greater than 6')
+            }
+            if (value.toLowerCase().includes("password")) {
+                throw new Error('Password must not contain "password"');
+            }
+        },
+        trim: true
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+module.exports = User
