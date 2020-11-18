a2cdc9cc4afe82e605d23739b093116378a30889
Satyam <satyambhardawaj@gmail.com>
2020-11-18 14:23:40 +0530

    setting up routers

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,128 +1,14 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    // user.save().then(() => {
-    //     res.status(201).send(user)
-    // }).catch((e) => {
-    //     res.status(400).send(e)
-    // })
-    try{
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req, res) => {
-    // User.find({}).then((users) => {
-    //     res.send(users)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-
-    try{
-        const userFind = await User.find({})
-        res.status(201).send(userFind)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try{
-        const userByID = await User.findById(_id)
-        if (!userByID) {
-            return res.status(404).send()
-        }
-
-        res.send(userByID)
-    } catch(e) {
-        res.status(500).send()
-    }
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-
-    // console.log(req.params);
-})
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
-    try{
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-
-    // task.save().then(() => {
-    //     res.status(201).send(task)
-    // }).catch((e) => {
-    //     res.status(400).send(e)
-    // })
-
-
-})
-
-app.get('/tasks', async (req, res) => {
-    
-    try{
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(404).send()
-    }
-    
-    // Task.find({}).then((tasks) => {
-    //     res.send(tasks)
-    // }).catch((e) => {
-    //     res.status(404).send()
-    // })
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try{
-        const task = await Task.findById(_id)
-        if (!task) {
-            return  res.status(404).send()
-        }
-        res.send(task)
-    }catch (e) {
-        res.status(500).send()
-    }
-
-    // Task.findById(_id).then((task) => {
-    //     if (!task) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
