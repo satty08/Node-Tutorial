e7c4692977ace7c33c39c9fd1a02b97278cf644b
Satyam <satyambhardawaj@gmail.com>
2020-11-28 10:10:41 +0530

    file upload

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,6 +18,36 @@ const port = process.env.PORT || 3000
 //     res.status(503).send('Site under Maintainance')
 // })
 
+const multer = require('multer')
+const upload = multer({
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word Document'))
+        }
+
+        cb(undefined, true)
+
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+    }
+})
+
+
+const errorMiddleware = (req, res, next) => {
+    throw new Error('From my middleware')
+}
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
 
 app.use(express.json())
 app.use(userRouter)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
 const User = require('../models/user')
+const multer = require('multer')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -147,6 +148,27 @@ router.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+})
+
+const profileImage = multer({
+    dest: 'avatar',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image file'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+
+router.post('/users/me/avatar', profileImage.single('avatar'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
 
 module.exports = router
