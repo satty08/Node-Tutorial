17ddacb76c9df72f678afdb6fe0a653159641571
Satyam <satyambhardawaj@gmail.com>
2020-12-09 15:03:59 +0530

    working with jest

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,33 @@
-test('Hello world!', () => {
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+
+test('should calculate total with tip', () => {
+    const total = calculateTip(10, 0.3)
+    expect(total).toBe(13)
+    // if (total != 13) {
+    //     throw new Error(`Total tip should be 13, got ${total}`)
+    // }
 
 })
 
-test('error', () => {
-    throw new Error('Failure!')
+test('with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12.5)
 })
+
+test('fahrehite to celsius', () => {
+    const cel = fahrenheitToCelsius(32)
+    expect(cel).toBe(0)
+})
+
+test('celcuis to fahrenhite', () => {
+    const fah = celsiusToFahrenheit(5)
+    expect(fah).toBe(41)
+})
+
+// test('Hello world!', () => {
+
+// })
+
+// test('error', () => {
+//     throw new Error('Failure!')
+// })
