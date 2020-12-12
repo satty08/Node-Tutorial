e2b4fe11f5d8486031d9cfc9c829056cc4962676
Satyam <satyambhardawaj@gmail.com>
2020-12-12 14:04:12 +0530

    jest integration

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,21 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+// afterEach(() => {
+//     console.log('afterEach');
+// })
 
 test('should signup a new user', async () => {
     await request(app).post('/users').send({
@@ -8,4 +24,18 @@ test('should signup a new user', async () => {
         password: 'Yayatika@0708',
         age: 25
     }).expect(201)
+})
+
+test('should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+test('should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'yayatik@exmaple.com',
+        password: 'toString,knc'
+    }).expect(400)
 })
